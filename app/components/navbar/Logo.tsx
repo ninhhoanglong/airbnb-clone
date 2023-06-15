116ba0e86feccfe9c12d1ed92afb15843aa00607
HoangLong <84828423+ninhhoanglong@users.noreply.github.com>
2023-06-16 01:26:38 +0700

    Categories UI

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const Logo = () => {
-  //   const router = useRouter();
+  const router = useRouter();
 
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="Logo"
-      className="hidden md:block cursor-pointer"
+      className="hidden cursor-pointer md:block"
       height="100"
       width="100"
       src="/images/logo.png"
