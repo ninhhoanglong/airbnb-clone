7b1cef77d151c364b04180db28e684135d92959c
HoangLong <84828423+ninhhoanglong@users.noreply.github.com>
2023-06-16 00:28:49 +0700

    Social Login (Google and Github)

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { User } from "@prisma/client";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
+import { SafeUser } from "@/app/types";
+
 interface NavbarProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   console.log(currentUser);
 
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
+    <div className="fixed z-10 w-full bg-white shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
